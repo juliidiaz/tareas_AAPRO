a0e882d25e4c243fac9c0802cab0c9a23a84a35b
juliidiaz <105684155+juliidiaz@users.noreply.github.com>
2023-03-03 19:50:03 -0300

    Add files via upload
    TAREA CLASE 10

---
+++ js/javascript.js
@@ -0,0 +1,17 @@
+let nombre = "Juliana";
+let apellido = "Diaz";
+
+console.log (nombre + " " + apellido)
+
+let num1 = 2;
+let num2 = 3;
+let num3 = 4;
+let suma = (num1 + num2);
+
+console.log (suma)
+
+let suma2 = (suma + num3);
+
+console.log (suma2)
+
+
