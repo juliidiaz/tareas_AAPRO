2be0b167877323295c06bd4fa9e162b3a779bf20
juliidiaz <105684155+juliidiaz@users.noreply.github.com>
2023-03-03 21:43:05 -0300

    Add files via upload
    TAREA CLASE 11 (primer tp)

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,5 @@
+//TP 10//
+
 let nombre = "Juliana";
 let apellido = "Diaz";
 
@@ -14,4 +16,30 @@ let suma2 = (suma + num3);
 
 console.log (suma2)
 
+//TP 11//
+
+let topping = prompt ("Ingrese el topping");
+let precioTopping;
+let precioHelado = 500;
+let montoFinal;
+
+if (topping == "oreo") {
+    precioTopping = 10;
+    console.log ("Este topping es Oreo");
+
+} else if (topping == "kitkat") {
+    precioTopping = 15;
+    console.log ("Este topping es kitkat");
+
+} else if (topping == "kinder") {
+    precioTopping = 25;
+    console.log ("Este topping es kinder");
+
+} else {
+    precioTopping =0;
+    console.log ("No tenemos este topping");}
+
+montoFinal = precioHelado + precioTopping;
+
+console.log ("El helado cuesta $", montoFinal);
 
